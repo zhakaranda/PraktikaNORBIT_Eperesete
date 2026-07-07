6aebe5d724da6f74b422feefc6c347c35479f55d
Жакаранда <korolyva06@yandex.ru>
2026-07-07 19:41:38 +0300

    Создан проект для следующего задания

---
+++ PraktikaNORBIT_DataTypes_Zadanie2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraktikaNORBIT_DataTypes_Zadanie2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
